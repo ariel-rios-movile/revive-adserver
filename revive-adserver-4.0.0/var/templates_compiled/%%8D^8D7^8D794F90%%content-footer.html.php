3239e5f86c530ef897d3f0999e0556e2f1609d42
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2016-10-24 17:57:52 -0300

    Adds initial content for adserver environment.

---
+++ revive-adserver-4.0.0/var/templates_compiled/%%8D^8D7^8D794F90%%content-footer.html.php
@@ -0,0 +1,5 @@
+<?php /* Smarty version 2.6.18, created on 2016-10-24 17:53:26
+         compiled from layout/content-footer.html */ ?>
+<?php if ($this->_tpl_vars['showContentFrame']): ?>
+</div>
+<?php endif; ?>
